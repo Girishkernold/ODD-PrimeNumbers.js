f42346f9ee45bcd16f99794e7290bec18de0467f
Girish Petla <98948157+Girishkernold@users.noreply.github.com>
2023-10-30 09:54:23 +0530

    Update ODD&Prime.js

--- ODD&Prime.js
+++ ODD&Prime.js
@@ -1,82 +1,86 @@
-//JavaScript program to find the one-digit difference numbers between 1 to 10 million
-//with the Class ODDprimeNum
-
+// A JavaScript program to find the one-digit difference numbers between 1 to 10 million
+/**
+ * ODDprimeNum is the class, that contains the functions and methods
+ */
 
 class ODDprimeNum{
-
     /**
-     * To check if the number is positive or not
+     * To check if the number matches the constraints
      * @param {Number} number
      */
-    check(number)
+    Numchecking(number)
     {
         if(number < 0)
         {
             console.log("Enter positive numbers.") //prints the statement when the number is not +ve
         }
-
+        if (typeof number!='number')
+        {
+            console.log("enter the integer data only !")
+        }
     }
-    
     /**
-     * To check if the 'number' is ODD number.
+     * To Ensure the 'number' is ODD number (One digit diff)
      * @param {Number} number
-     * @returns true if the number is ODD number
+     * @returns true if the number is ODD number (One digit diff)
      */
-    isODDNumber(number){
-        this.check(number)
-
-        //number less than 10 is not a ODD number.
-        if(number < 10){
+    ODDNumber(number)
+    {
+        this.Numchecking(number)
+        if(number < 10)  //returns false when number is <10
+        {
             return false
         }
 
-        let n = number.toString()
+        let x = number.toString()
 
-        for(let i=0; i<n.length-1; i++){
-            if(Math.abs(Number(n[i]) - Number(n[i+1])) !== 1){
+        for(let i=0; i<x.length-1; i++)
+        {
+            if(Math.abs(Number(x[i]) - Number(x[i+1])) !== 1)
+            {
                 return false
             }
         }
         return true
     }
-
     /**
-     * To check if the 'number' is prime or not.
-     * @param {Number} number ,the number we need to check.
-     * @returns true ,If the number is prime.
+     * method of checking prime number.
+     * @param {Number} number
+     * @returns true when it is prime
      */
-    isPrime(number){
-
-        //2 is the only even prime number.
-        if(number == 2){
+    Prime(number){
+        if(number == 2)
+        {
             return true
         }
 
-        //Every other even number is not prime.
-        if(number%2 == 0){
+        if(number%2 == 0) //if num is div by 2 then it is not a prime
+        {
             return false
         }
 
-        //Every number has it's factors till the square root of the number.
-        for(let i=3; i<=Math.floor(Math.sqrt(number)); i+=2){
-            if(number%i === 0){
+        for(let i=3; i<=Math.floor(Math.sqrt(number)); i+=2)
+        {
+            if(number%i === 0)
+            {
                 return false
             }
         }
         return true
     }
 }
-
+/**
+ * here is the function to print the ODD numbers
+ */
 function PrintNums()
 {
 
-        let oddPrime = [] 
-        ODD = new ODDprimeNum()
+        let oddPrime = []  //empty list is created
+        ODD = new ODDprimeNum()  //class is assigned to the variable ODD
 
-        
         for(let Givenrange = 0; Givenrange <= 10000000; Givenrange++)
         {  
-            if(ODD.isODDNumber(Givenrange) && ODD.isPrime(Givenrange))
+            if(ODD.ODDNumber(Givenrange) && ODD.Prime(Givenrange))  // both methods are included
             {
                 oddPrime.push(Givenrange)
             }
@@ -85,8 +89,6 @@ function PrintNums()
         console.log(`\nThe count of numbers are :${oddPrime.length}`)
         console.log(oddPrime)
 
-
-
 }
 
-PrintNums()
+PrintNums()  //function called and it performs the entire  operations
